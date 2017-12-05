11285fd8949a64d207c3f101e3ccbddd888d6fe6
Mark Davis <markdavis@Marks-MacBook-Pro.local>
2017-12-05 11:11:12 -0500

    fix code climate issues, again

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,8 +1,7 @@
+# This is the venues controller
 class VenuesController < ApplicationController
-  #provide definitions for controller actions
-  def index
-  end
+  # provide definitions for controller actions
+  def index; end
 
-  def show
-  end
+  def show; end
 end
