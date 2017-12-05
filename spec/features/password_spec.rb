315c38e8baf1d10b76cec22befa6190ba881c3d6
Jason Prestosh <jason.prestosh@gmail.com>
2017-12-05 15:32:46 -0500

    Fixed Code Climate errors

--- spec/features/password_spec.rb
+++ spec/features/password_spec.rb
@@ -1,34 +1,30 @@
 require 'rails_helper'
 
-feature "reset password" do
+feature 'reset password' do
   let!(:user) do
-    User.create(first_name: "John",
-      last_name: "Smith",
-      email: "john@firstenglishcolony.edu",
-      password: "pocahontas")
+    User.create(
+      first_name: 'John',
+      last_name: 'Smith',
+      email: 'john@firstenglishcolony.edu',
+      password: 'pocahontas')
     end
-  scenario "user clicks forgot password" do
-
+    
+  scenario 'user clicks forgot password' do
     visit new_user_session_path
+    click_link 'Forgot your password?'
 
-    click_link "Forgot your password?"
-
-    expect(current_path).to eq "/users/password/new"
+    expect(current_path).to eq '/users/password/new'
   end
-  scenario "user resets password" do
-
+  scenario 'user resets password' do
     visit new_user_session_path
+    click_link 'Forgot your password?'
+    fill_in 'Email', with: 'john@firstenglishcolony.edu'
+    click_button 'Send me reset password instructions'
 
-    click_link "Forgot your password?"
+    expect(current_path).to eq '/users/sign_in'
+    expect(page).to have_content 'You will receive an email with instructions
+     on how to reset your password in a few minutes.
 
-    fill_in "Email", with: "john@firstenglishcolony.edu"
-
-    click_button "Send me reset password instructions"
-
-    expect(current_path).to eq "/users/sign_in"
-
-    expect(page).to have_content "You will receive an email with instructions on how to reset your password in a few minutes.
-
-"
+'
   end
 end
--- spec/features/registration_tests_spec.rb
+++ spec/features/registration_tests_spec.rb
@@ -1,32 +1,29 @@
 require 'rails_helper'
 
-feature "visitor registers as new user" do
-  scenario "user sees sign up button on home page" do
+feature 'visitor registers as new user' do
+  scenario 'user sees sign up button on home page' do
     pending
     visit root_path
 
-    expect(page).to have_content "Sign up"
+    expect(page).to have_content 'Sign up'
   end
 
-  scenario "user sucessfully fills out registration form" do
-
+  scenario 'user sucessfully fills out registration form' do
     visit new_user_registration_path
 
-    fill_in 'First Name', with: "John"
-    fill_in 'Last Name', with: "Smith"
-    fill_in 'Email', with: "john@firstenglishcolony.edu"
-    fill_in 'Password', with: "pocahontas"
-    fill_in 'Password Confirmation', with: "pocahontas"
+    fill_in 'First Name', with: 'John'
+    fill_in 'Last Name', with: 'Smith'
+    fill_in 'Email', with: 'john@firstenglishcolony.edu'
+    fill_in 'Password', with: 'pocahontas'
+    fill_in 'Password Confirmation', with: 'pocahontas'
 
     click_button 'Sign up'
 
     expect(current_path).to eq '/'
   end
 
-  scenario "user unsucessfully fills out registration form" do
-
+  scenario 'user unsucessfully fills out registration form' do
     visit new_user_registration_path
-
     click_button 'Sign up'
 
     expect(page).to have_content "Email can't be blank"
@@ -35,19 +32,20 @@ feature "visitor registers as new user" do
     expect(page).to have_content "Last name can't be blank"
   end
 
-  scenario "user edits their account" do
+  scenario 'user edits their account' do
+    user = User.create(
+      first_name: 'John',
+      last_name: 'Smith',
+      email: 'john@firstenglishcolony.edu',
+      password: 'pocahontas')
 
-    user = User.create(first_name: "John",
-      last_name: "Smith",
-      email: "john@firstenglishcolony.edu",
-      password: "pocahontas")
-      login_as(user, :scope => :user)
+    login_as(user, scope: :user)
 
     visit edit_user_registration_path
 
-    expect(page).to have_content "Edit"
-    expect(find_field("First Name").value).to eq "John"
-    expect(find_field("Last Name").value).to eq "Smith"
-    expect(find_field("Email").value).to eq "john@firstenglishcolony.edu"
+    expect(page).to have_content 'Edit'
+    expect(find_field('First Name').value).to eq 'John'
+    expect(find_field('Last Name').value).to eq 'Smith'
+    expect(find_field('Email').value).to eq 'john@firstenglishcolony.edu'
   end
 end
