f1d9e823e7aa84aee8cb409f01f5e4388dff467f
Jason Prestosh <jason.prestosh@gmail.com>
2017-12-05 16:27:13 -0500

    Review unit tests in progress

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,12 @@
 class Review < ApplicationRecord
+  belongs_to :venue
+  # Reactivate once merged with add_devise branch:
+  # belongs_to :user
+
+  validates_presence_of :rating, :user_id, :venue_id
+
+  validates :rating,
+    numericality: true,
+    inclusion: 1..5
+
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_valid(:rating).when(1,2,3,4,5) }
+  it { should_not have_valid(:rating).when(nil, '')}
 end
