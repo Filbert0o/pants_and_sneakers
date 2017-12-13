d0206f24de27fb92f5288c4c157a17f0a634e656
Timothy Andrew Filbert <timfilbert97@gmail.com>
2017-12-13 18:39:42 -0500

    Added Pagination

--- react/src/containers/VenuesIndexContainer.js
+++ react/src/containers/VenuesIndexContainer.js
@@ -9,9 +9,12 @@ class VenuesIndexContainer extends Component {
     super(props);
     this.state = {
       venues: [],
-      currentUser: null
+      currentUser: null,
+      currentPage: 1,
+      venuesPerPage: 5
     }
     this.handleClick = this.handleClick.bind(this)
+    this.handlePagination = this.handlePagination.bind(this)
     this.getVenues = this.getVenues.bind(this)
   }
 
@@ -47,9 +50,39 @@ class VenuesIndexContainer extends Component {
     browserHistory.push('/venues/new')
   }
 
+  handlePagination(event) {
+    this.setState({
+      currentPage: Number(event.target.id)
+    })
+  }
+
 
   render() {
-    let venues = this.state.venues.map((venue) => {
+    // let venues = this.state.venues.map((venue) => {
+    //   return(
+    //     <VenuesIndexTile
+    //       key={venue.id}
+    //       id={venue.id}
+    //       name={venue.name}
+    //       address={venue.address}
+    //       city={venue.city}
+    //       state={venue.state}
+    //       zip={venue.zip}
+    //       imageUrl={venue.image_url}
+    //     />
+    //   )
+    // })
+    let button;
+    if (!!this.state.currentUser) {
+      button = <button><Link to={`/venues/new`}>Submit A New Venue</Link></button>
+    }
+
+    // Logic for displaying Venues
+    const indexOfLastVenue = this.state.currentPage * this.state.venuesPerPage;
+    const indexOfFirstVenue = indexOfLastVenue - this.state.venuesPerPage;
+    const currentVenues = this.state.venues.slice(indexOfFirstVenue, indexOfLastVenue);
+
+    let venues = currentVenues.map((venue) => {
       return(
         <VenuesIndexTile
           key={venue.id}
@@ -63,15 +96,32 @@ class VenuesIndexContainer extends Component {
         />
       )
     })
-    let button;
-    if (!!this.state.currentUser) {
-      button = <button><Link to={`/venues/new`}>Submit A New Venue</Link></button>
+
+    // Logic for displaying page numbers
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(this.state.venues.length / this.state.venuesPerPage); i++) {
+      pageNumbers.push(i);
     }
+    const renderPageNumbers = pageNumbers.map(number => {
+      return (
+        <li
+          className='button'
+          key={number}
+          id={number}
+          onClick={this.handlePagination}
+        >
+          {number}
+        </li>
+      )
+    })
 
     return(
       <div className='row'>
         {button}
         {venues}
+        <ul className='page-numbers'>
+          {renderPageNumbers}
+        </ul>
       </div>
     )
   }
