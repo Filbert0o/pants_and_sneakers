4f252e9afd19a00b9c963c801599cd80db023cd8
Joe Ziemba <jziemba90@gmail.com>
2017-12-13 18:06:11 -0500

    added admin/user_controller tests

---
+++ spec/controllers/admin/users_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe Admin::UsersController, type: :controller do
+  describe '#index' do
+    it 'should render the admin/index template' do
+      admin = create(:user, role: 'admin')
+      sign_in admin
+
+      get :index
+      assert_response :success
+    end
+  end
+end
