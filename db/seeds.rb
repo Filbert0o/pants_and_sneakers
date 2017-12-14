0ded87c3ac53aaae52179eb18dc357a9fba9a323
Mark Davis <markdavis.070@gmail.com>
2017-12-14 17:00:13 -0500

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,15 @@ venues = [
     city: "Philadelphia",
     state: "PA",
     zip: "19123",
+    website: "www.utphilly.com",
+    age_restriction: "21+",
+    food_options: "Limited Menu",
+    parking: true,
+    hours: "Wheneva!",
+    phone: "215-232-2100"
+    dress_code: "Come as you are!",
+    cover_charge: "YES",
+    cash_only: false,
     image_url: "https://slack-imgs.com/?c=1&url=http%3A%2F%2Finyourspeakers.com%2Ffiles%2Fimagecache%2Fgallery_full%2Fchromeo-2web.jpg"
   },
   {
@@ -23,6 +32,15 @@ venues = [
     city: "Philadlephia",
     state: "PA",
     zip: "19123",
+    website: "www.thefillmorephilly.com/",
+    age_restriction: "21+",
+    food_options: "Limited Menu",
+    parking: true,
+    hours: "Late, and then early!",
+    phone: "215-309-0150 "
+    dress_code: "Casual!",
+    cover_charge: "YES",
+    cash_only: false,
     image_url: "https://www.wheretraveler.com/sites/default/files/styles/promoted_image_social_large/public/The%20Fillmore%20Music%20Hall.jpg?itok=GR_E89B9"
   },
   {
@@ -31,14 +49,32 @@ venues = [
     city: "Philadlephia",
     state: "PA",
     zip: "19107",
+    website: "www.thetroc.com",
+    age_restriction: "21+",
+    food_options: "None",
+    parking: true,
+    hours: "Vampire hours!",
+    phone: "215-922-6888"
+    dress_code: "Scuzzy!",
+    cover_charge: "YES",
+    cash_only: false,
     image_url: "http://fast.clickbooq.com/4bbb00-380/HippieSabotageDB1-lg.jpg"
   },
   {
-    name: "Tattoed Mom",
+    name: "Tattooed Mom",
     address: "504 South St.",
     city: "Philadelphia",
     state: "PA",
     zip: "19101",
+    website: "www.tattooedmomphilly.com",
+    age_restriction: "21+",
+    food_options: "Limited Menu",
+    parking: true,
+    hours: "All dang day...!",
+    phone: "215-238-9880"
+    dress_code: "Come as you are!",
+    cover_charge: "Depends",
+    cash_only: false,
     image_url: "http://3.bp.blogspot.com/--QKLe18pNqI/UOSgbZ8jk6I/AAAAAAAAAqw/Rdk0m2s9RuQ/s1600/Home+Sweet+Home+Fan+Install.jpg"
   }
 ]
