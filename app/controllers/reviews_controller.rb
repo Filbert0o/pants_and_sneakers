a6cc6aa8611e7958b0b753ae77d4b50a877e6a32
Joe Ziemba <jziemba90@gmail.com>
2017-12-14 10:37:09 -0500

    reviews_controller test

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,4 @@
 # Reviews Controller
 class ReviewsController < ApplicationController
+  def new; end
 end
--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 describe ReviewsController, type: :controller do
-  it 'should exist' do
+  it 'should have a new method' do
+    venue = create(:venue)
+    get :new, params: { venue_id: venue.id }
+    assert_response :success
   end
 end
